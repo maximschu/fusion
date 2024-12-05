2db398a534bca3e17ab087f1075041dd92026b42
maximschu <102390365+maximschu@users.noreply.github.com>
2024-12-05 05:28:24 +0000

    Add files via upload

--- Fusion/fusion.py
+++ Fusion/fusion.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
+from collections import defaultdict
 
 
-xthetad = -90
-ythetad = 180
+xthetad = 90
+ythetad = 0
 zthetad = 0
 
 xtheta = np.radians(xthetad)
@@ -84,32 +85,90 @@ print(lidarset)
 cameradf = cameradf.sort_values(by=['t'])
 lencameradf = len(cameradf)
 
-# 100ms (in ns)
-tthreshold = 10000000000
+# 100ms (in s and ns)
+tthresholds = 0.1
+tthreshold = tthresholds * pow(10,9)
+
+fusiondict = defaultdict(list)
 
 for id in lidarset:
     iddf = lidardf.loc[lidardf['ObjectID'].eq(id)]
+
 
     for index, row in iddf.iterrows():
         t = row["Timestamp"]*pow(10,9)
         
         camerat = cameradf["t"].to_list()
         lencamerat = len(camerat)
-        cameraidx = int(lencamerat/2)
-        print(lencamerat)
-        print(cameraidx)
-        print(camerat[600])
 
-        while True:
-            if (camerat[cameraidx] - tthreshold) <= t <= (camerat[cameraidx]):
-                print("match")
+        #print("-----------------")
+        #print(f"Target t: {t:.0f}")
+        #print("-----------------")
+
+        matchFound = False
+        cameramatcht = 0
+
+        maxidx = len(camerat)
+        minidx = 0
+
+        while (maxidx - minidx) > 1:
+            mididx = int((maxidx+minidx)/2)
+
+            if (camerat[mididx] - tthreshold) <= t <= (camerat[mididx] + tthreshold):
+                #print("MATCH FOUND")
+                #print("DELTA T (s) == " + str(abs((t-camerat[mididx])/pow(10,9))))
+                matchFound = True
+                cameramatcht = camerat[mididx]
                 break
-            elif ((camerat[cameraidx] - tthreshold) < t):
-                camerat = camerat[:len(camerat)//2]
-                lencamerat = len(camerat)
-                cameraidx = int(lencamerat/2)
-            elif ((camerat[cameraidx] + tthreshold) > t):
-                camerat = camerat[len(camerat)//2:]
-                lencamerat = len(camerat)
-                cameraidx = int(lencamerat/2)
-    
+
+            elif ((camerat[mididx] - tthreshold) < t):
+                minidx = mididx
+
+            elif ((camerat[mididx] + tthreshold) > t):
+                maxidx = mididx
+            
+
+        
+        if matchFound:
+            threshold = 0.1
+            thresholdns = threshold * pow(10,9)
+
+            left = mididx
+            right = left
+
+            while (cameramatcht - camerat[left]) < thresholdns:
+                left -= 1
+                if left < 1:
+                    break
+            
+            while (camerat[right] - cameramatcht) < thresholdns:
+                right += 1
+                if right >= len(camerat) - 1:
+                    break
+            
+            matchdf = cameradf[left:right]
+            matchdf = matchdf.sort_values(by=['ID'])
+
+            #print(matchdf)
+
+            matchlist = []
+
+            for unused, camerarow in matchdf.iterrows():
+                a = np.array((camerarow['x'], camerarow['y']))
+                b = np.array((row['PositionX'], row['PositionY']))
+                dist = np.linalg.norm(a - b)
+
+                # Threshold in m
+                distthr = 1
+
+                if dist <= distthr:
+                    matchlist.append(camerarow['ID'])
+
+                #print(f"Id: {id}  ---  dist:{dist}")
+            
+            if matchlist:
+                fusiondict[id].append(matchlist)
+
+print(fusiondict)
+
+
